21e0870e4f3f069a4706a87b0139bd965c30e218
banbooz <banboozplays@gmail.com>
2026-06-24 21:33:15 +0100

    Load mobile browser layout fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import "./iphone-fixes.css";
 import { APP_NAME, APP_VERSION } from "./lib/appMeta";
 
 export const metadata: Metadata = {
